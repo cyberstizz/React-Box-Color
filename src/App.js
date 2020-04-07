bb03ec71209b1e75a1ecdf8bfa0e52a44b20b302
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-07 11:46:18 -0400

    added and binded function to change box

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   constructor(props){
     super(props)
+    // determining state which will start at orange
     this.state = {
       color: 'orange',
       count:  0
     }
+    //binding my click function within the constructor. this function will be created soon
+    this.clicks = this.clicks.bind(this)
+  }
+  // creating a function that will execute upon a click. This function will change box color
+  // and update the count
+  clicks(){
+    let color = this.state.color
+    let counter = this.state.counter
+    if(color === 'orange'){
+      color = 'blue'
+    } else if(color === 'blue'){
+      color = 'orange'
+    }
+    counter += 1;
   }
   render(){
   return (
