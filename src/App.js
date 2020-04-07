b10d9792110145775813feca2ef58c69ad0da3bb
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-07 13:22:35 -0400

    added functionality but it is not working yet

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     // determining state which will start at orange
     this.state = {
       color: 'orange',
-      count:  0
+      count: 0
     }
     //binding my click function within the constructor. this function will be created soon
     this.clicks = this.clicks.bind(this)
@@ -15,24 +15,30 @@ class App extends React.Component {
   // creating a function that will execute upon a click. This function will change box color
   // and update the count
   clicks(){
-    let color = this.state.color
-    let counter = this.state.counter
-    //box color will change to either blue or orange when clicked
-    if(color === 'orange'){
-      color = 'blue'
-    } else if(color === 'blue'){
-      color = 'orange'
-    }
-    counter += 1;
+        //box color will change to either blue or orange when clicked
+
+    const colorSwitch = () =>{
+      if(this.state.color === 'orange'){
+        this.state.color = 'blue'
+      } else if(this.state.color === 'blue'){
+        this.state.color = 'orange'
+      }
+      //setting a counter to represent the counter state
+    let counter = this.state.count;
+    //setting the state to change color and increment by one
+    this.setState({
+      color: colorSwitch,
+      count: counter + 1
+    })
   }
-  render(){
-  return (
-    
-    <div className="App">
-      <div id='box'></div>
-    </div>
-  );
-}
 }
 
-export default App;
+  render() {
+  return (
+    <div className="App">
+      <div id='box' onClick={this.props.clicks}></div>
+    </div>
+  )
+  }
+}
+export default App
