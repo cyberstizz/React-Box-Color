21f8316fb89f3228ed12900c57d7531c69aeae13
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-11 20:18:30 -0400

    more fixes

--- src/App.js
+++ src/App.js
@@ -10,33 +10,40 @@ class App extends React.Component {
       count: 0
     }
     //binding my click function within the constructor. this function will be created soon
-    this.clicks = this.clicks.bind(this)
+    this.click = this.click.bind(this)
   }
   // creating a function that will execute upon a click. This function will change box color
   // and update the count
-  clicks(){
+  click(){
         //box color will change to either blue or orange when clicked
 
     const colorSwitch = () =>{
       if(this.state.color === 'orange'){
-        this.state.color = 'blue'
+        return this.state.color = 'blue'
       } else if(this.state.color === 'blue'){
-        this.state.color = 'orange'
+        return this.state.color = 'orange'
       }
+    }
       //setting a counter to represent the counter state
     let counter = this.state.count;
     //setting the state to change color and increment by one
+
+    const changer = () =>{
+
+    
     this.setState({
       color: colorSwitch,
       count: counter + 1
     })
+    changer();
   }
-}
+  }
+
 
   render() {
   return (
     <div className="App">
-      <div id='box'  style={{backgroundColor: this.state.color}} onClick={this.props.clicks}></div>
+      <div id='box' style={{backgroundColor: this.state.color}} onClick={this.props.click}></div>
     </div>
   )
   }
